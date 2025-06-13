e84772a0359e80d8053b58d13749b532f545d70e
Aashmaya-J <aashmayakannan@gmail.com>
2025-06-13 15:12:57 +0530

    Update task1.java

--- day1task/task1.java
+++ day1task/task1.java
@@ -1,8 +1,33 @@
 package day1;
-
+import java.util.Scanner;
 public class task1 {
-	public static void main(String[]args)
-	{
-		System.out.println("welcome");
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size + 1];
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.print("Enter the value to insert: ");
+        int value = scanner.nextInt();
+        System.out.print("Enter the position (0 to " + size + "): ");
+        int position = scanner.nextInt();
+        if (position < 0 || position > size) {
+            System.out.println("Invalid position!");
+        } else {
+            for (int i = size; i > position; i--) {
+                array[i] = array[i - 1];
+            }
+            array[position] = value;
+            size++;
+            System.out.println("Array after insertion:");
+            for (int i = 0; i < size; i++) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        scanner.close();
+    }
 }
+
